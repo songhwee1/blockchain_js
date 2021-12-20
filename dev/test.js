1a4ca9cbab99059f0966c46d0131aa84087ba711
songhwee1 <songhwee1@naver.com>
2021-12-20 09:47:39 +0900

    add createNewTransaction func

--- dev/test.js
+++ dev/test.js
@@ -3,7 +3,9 @@ const Blockchain = require('./blockchain');
 const bitcoin = new Blockchain();
 
 bitcoin.createNewBlock(2389,'OIUOEREDHKHKD','78s97d4x6dsf'); //임의의 값을 넣어서 새로운 블록 생성
-bitcoin.createNewBlock(2464,'WAEHGOWAEWGEW','8awe9gwe9beb'); //임의의 값을 넣어서 새로운 블록 생성
-bitcoin.createNewBlock(6489,'IEBEBIEBWOBEZ','3g32ogi23ob3'); //임의의 값을 넣어서 새로운 블록 생성
+//bitcoin.createNewBlock(2464,'WAEHGOWAEWGEW','8awe9gwe9beb'); //임의의 값을 넣어서 새로운 블록 생성
+//bitcoin.createNewBlock(6489,'IEBEBIEBWOBEZ','3g32ogi23ob3'); //임의의 값을 넣어서 새로운 블록 생성
+
+bitcoin.createNewTransaction(100,'HWI013801GGHWEG','YEON4198149GLNL');
 
 console.log(bitcoin)
